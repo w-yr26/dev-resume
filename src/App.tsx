82639fefd29218e12c75d32ff6f8919fc482cf06
wyr <18026086011@163.com>
2025-03-15 08:48:12 +0800

    feat: 国际化切换zh

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 // import './App.css'
 import { Outlet } from 'react-router-dom'
+import { ConfigProvider } from 'antd'
+import zhCN from 'antd/locale/zh_CN' // 引入中文语言包
+
 function App() {
   return (
-    <>
+    <ConfigProvider locale={zhCN}>
       <Outlet></Outlet>
-    </>
+    </ConfigProvider>
   )
 }
 
