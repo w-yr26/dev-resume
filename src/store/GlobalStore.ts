d4df22e56007a1ccc544267f72d4e96948ff8a53
wyr <18026086011@163.com>
2025-03-30 20:46:44 +0800

    feat: 工作经历模块

--- src/store/GlobalStore.ts
+++ src/store/GlobalStore.ts
@@ -2,13 +2,14 @@ import { create } from 'zustand'
 import type { GlobalInitType } from '@/types/global'
 import BaseInfoTem from '@/templates/BaseInfoTem'
 import ProjectTem from '@/templates/ProjectTem'
+import WorkTem from '@/templates/WorkTem/index'
 
 const useGlobalStore = create<GlobalInitType>(() => {
   return {
     keyToComponentMap: {
       BASE_INFO: BaseInfoTem,
       EDU_BG: ProjectTem,
-      // WORK_EXP: WorkExperience,
+      WORK_EXP: WorkTem,
       // PROJECT_EXP: ProjectExperience,
       // AWARD_LIST: Award,
       // SKILL_LIST: Skill,
