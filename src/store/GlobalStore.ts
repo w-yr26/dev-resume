ed618c91687956f5557ff82dd5906c9c3561441c
wyr <18026086011@163.com>
2025-06-14 11:19:13 +0800

    fix: 删除多余代码

--- src/store/GlobalStore.ts
+++ src/store/GlobalStore.ts
@@ -1,25 +1,9 @@
 import { create } from 'zustand'
 import type { GlobalInitType } from '@/types/global'
-import BaseInfoTem from '@/templates/BaseInfoTem'
-import ProjectTem from '@/templates/ProjectTem'
-import WorkTem from '@/templates/WorkTem/index'
-import SkillTem from '@/templates/SkillTem'
-import EduBgTem from '@/templates/EduBgTem'
-import HeartTem from '@/templates/HeartTem'
-import AwardTem from '@/templates/AwardTem'
 import { produce } from 'immer'
 
 const useGlobalStore = create<GlobalInitType>((set) => {
   return {
-    keyToComponentMap: {
-      BASE_INFO: BaseInfoTem,
-      EDU_BG: EduBgTem,
-      WORK_EXP: WorkTem,
-      PROJECT_EXP: ProjectTem,
-      AWARD_LIST: AwardTem,
-      SKILL_LIST: SkillTem,
-      HEART_LIST: HeartTem,
-    },
     keyToPosition: {
       // BASE_INFO: 0,
     },
